f10047039ae3759e41db74f54e9075294a16efc6
Khanh Bui <kbuiblue@gmail.com>
2024-09-01 19:51:40 +0700

    feat: set up Grid and GridSquare

---
+++ src/components/Grid.vue
@@ -0,0 +1,16 @@
+<script setup>
+import GridSquare from "./GridSquare.vue";
+import { reactive } from "vue";
+
+const gridState = reactive({
+    row1: [null, null, null],
+    row2: [null, null, null],
+    row3: [null, null, null],
+});
+</script>
+
+<template>
+    <div class="grid grid-cols-3 grid-rows-3 w-2/3 h-[80vh] m-auto">
+        <GridSquare v-for="(key, value) in gridState" :row="key" :id="value"></GridSquare>
+    </div>
+</template>
---
+++ src/components/GridSquare.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    row: Array,
+    id: String,
+});
+</script>
+
+<template>
+    <div
+        v-for="(value, index) in row"
+        :key="`${id}-${index}`"
+        :id="`${id}-${index}`"
+        class="border border-black"
+    >
+        <p v-if="value === null">It is null</p>
+    </div>
+</template>
