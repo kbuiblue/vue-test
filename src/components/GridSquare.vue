4f8f2bdf29f951e677d5dbb1cf239625ffa1c8bc
Khanh Bui <kbuiblue@gmail.com>
2024-09-09 19:40:40 +0700

    feat: init gameState and updateGameState

--- src/components/GridSquare.vue
+++ src/components/GridSquare.vue
@@ -1,8 +1,11 @@
 <script setup>
+
 defineProps({
     row: Array,
     id: String,
 });
+
+defineEmits(["updateGameState"])
 </script>
 
 <template>
@@ -10,6 +13,7 @@ defineProps({
         v-for="(value, index) in row"
         :key="`${id}-${index}`"
         :id="`${id}-${index}`"
+        @click="$emit('updateGameState')"
         class="border border-black"
     >
         <p v-if="value === null">It is null</p>
