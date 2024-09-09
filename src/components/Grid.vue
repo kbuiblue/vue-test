29af242b4ecd7070f3876a00a33b2858c1b4afee
Khanh Bui <kbuiblue@gmail.com>
2024-09-09 19:40:40 +0700

    feat: init gameState and updateGameState

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -7,10 +7,32 @@ const gridState = reactive({
     row2: [null, null, null],
     row3: [null, null, null],
 });
+
+const gameState = reactive({
+    gameEnded: false,
+    winner: null,
+    players: ["X", "O"],
+    playerTurn: null,
+});
+
+// set initial gamestate
+if (!gameState.gameEnded && !gameState.playerTurn) {
+    gameState.playerTurn = gameState.players[0];
+}
+
+const updateGameState = () => {
+    gameState.playerTurn = gameState.playerTurn === "X" ? "O" : "X";
+    console.log(gameState.playerTurn)
+};
 </script>
 
 <template>
     <div class="grid grid-cols-3 grid-rows-3 w-2/3 h-[80vh] m-auto">
-        <GridSquare v-for="(key, value) in gridState" :row="key" :id="value"></GridSquare>
+        <GridSquare
+            v-for="(key, value) in gridState"
+            @update-game-State="updateGameState"
+            :row="key"
+            :id="value"
+        ></GridSquare>
     </div>
 </template>
